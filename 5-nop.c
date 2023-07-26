bb4049c50cd81d03afd4f1df8bc2214ed5c232d7
Obare <obaredafiaghor@gmail.com>
2023-07-26 15:26:24 -0700

    stacks

---
+++ 5-nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  *f_nop- represents nil
+  *@head: head of stack
+  *@counter: line_number
+  *Return: nothing
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
